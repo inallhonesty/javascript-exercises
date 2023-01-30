a1bfc6d0f8e9464d99bc405038d24d3a3a3c1720
Paul <inallhonesty92@gmail.com>
2023-01-30 16:22:42 +0200

    Solve exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,24 @@
-const findTheOldest = function() {
+const findTheOldest = function(lst) {
+    const currentYear = new Date().getFullYear();
+    const oldest = lst.reduce(function(old, current) {
+        if (!old['yearOfDeath']) {
+            old['yearOfDeath'] = currentYear
+        } 
+        if (!current['yearOfDeath']) {
+            current['yearOfDeath'] = currentYear
+        }
 
+        oldAge = old['yearOfDeath'] - old['yearOfBirth']
+        currentAge = current['yearOfDeath'] - current['yearOfBirth']
+        if (oldAge - currentAge >= 0) {
+            return old
+        } else {
+            return current
+        }
+    })
+    return oldest
 };
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
