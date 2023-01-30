854a9e997d26f9083e6048e241ad8c16df3db8b5
Paul <inallhonesty92@gmail.com>
2023-01-30 16:02:11 +0200

    Solve exercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,22 @@
-const fibonacci = function() {
+const fib = {
+    0: 0,
+    1: 1,
+}
 
-};
+const fibonacci = function(a) {
+    a = Number(a)
+    if (a < 0) {
+        return "OOPS"
+    }
 
+    while (a > 0) {
+        if (fib[a]) return fib[a];
+
+        fib[a] = fibonacci(a-1) + fibonacci(a-2);
+    }
+
+    return fib[a];
+
+}
 // Do not edit below this line
 module.exports = fibonacci;
