a61d7bf57d82ebd83172881ab578b87fe7afa6eb
Paul <inallhonesty92@gmail.com>
2023-01-30 13:23:17 +0200

    Solve exercise number 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let letterOnly = Array.from(str).filter(char => char.match(/[a-zA-Z]/)).join('');
+    letterOnly = letterOnly.toLowerCase()
+    let revStr = Array.from(letterOnly).reverse().join("");
+    return letterOnly === revStr
 };
 
 // Do not edit below this line
